42b32cf778c7e5d4fcbb4a108c3061a33da6f5ca
ziasana <zs.hossainy@gmail.com>
2025-12-18 14:10:51 +0100

    eslint pre commit configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
     images: {
-        domains: ['images.unsplash.com'], // Add your external host here
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                pathname: "/**",
+            },
+        ],
     },
   /* config options here */
    /* async rewrites() {
